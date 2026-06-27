6e7e0a891b316293c7f35966e7bb70e405d7ab3b
KafuChino <3292440334@qq.com>
2026-06-27 21:13:31 +0800

    Update tg_panel.py

--- tg_panel.py
+++ tg_panel.py
@@ -13,7 +13,7 @@ import time
 # 【版本定义】
 # 每次修改代码推送到 GitHub 前，请手动提升此版本号
 # ==========================================
-CURRENT_VERSION = "v1.3.1"
+CURRENT_VERSION = "v1.3.2"
 
 CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.json')
 SESSION_FILE = os.path.join(os.path.dirname(__file__), 'api_auth.session')
